2599dd80969ad68f3b47cc1652e1ebeaebd1d654
rickywu1999 <rickywu1999@gmail.com>
2016-03-11 01:36:46 -0500

    finished MergeSortTester (by myself)

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,4 +1,4 @@
-//Ricky Wu, Vincent Tang
+//Ricky Wu
 //APCS2 pd10
 //HW11 -- Wrapping It Up
 //2016-03-09
@@ -42,13 +42,19 @@
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  n=8       time:5500 ns 
+  n=16      time:12000 ns 
+  n=32      time:26000 ns 
+  n=64      time:55000 ns
+  n=128     time:130000 ns 
   ...
-  n=<huge>  time: 
+  n=x       time:roughly 75x * log2(75x)
+  
+  NOTE: results may vary from computer to computer
+
   ANALYSIS:
-  <INSERT YOUR RESULTS ANALYSIS HERE>
+  From the results, you can see that the method's runtime is indeed O(nlog(n)). When the length of the list doubles, the runtime is roughly doubled as well
+  The reason I didn't include when n is 1,2, or 4 is because they didn't follow the rules for some reason.
   ======================================*/
 
 public class MergeSortTester {
@@ -56,44 +62,68 @@ public class MergeSortTester {
 	long time1 = 0;
 	long time2 = 0;
 	MergeSort merge = new MergeSort();
-	int[] test1 = {4};
-	int[] test2 = {4,2,4,5,1,2,3,9,123,12};
-	int[] test3 = new int[100];
-	for (int i = 0; i < test3.length; i++){
-	    test3[i] = (int)(Math.random() * 100);
-	}
-	int[] test4 = new int[1000];
+	int[] test1 = {4,2};
+	int[] test2 = {4,2,4,5};
+	int[] test3 = {4,2,4,5,1,2,76,2};
+	int[] test4 = new int[16];
 	for (int i = 0; i < test4.length; i++){
 	    test4[i] = (int)(Math.random() * 100);
 	}
-	int[] test5 = new int[10000];
+	int[] test5 = new int[32];
 	for (int i = 0; i < test5.length; i++){
 	    test5[i] = (int)(Math.random() * 100);
 	}
-	   
+	int[] test6 = new int[64];
+	for (int i = 0; i < test6.length; i++){
+	    test6[i] = (int)(Math.random() * 100);
+	}
+	int[] test7 = new int[128];
+	for (int i = 0; i < test7.length; i++){
+	    test7[i] = (int)(Math.random() * 100);
+	}
+	int[] test8 = new int[(int)Math.pow(2,10)];
+	for (int i = 0; i < test8.length; i++){
+	    test8[i] = (int)(Math.random() * 100);
+	}
+	
 	time1 = System.nanoTime();
 	merge.sort(test1);
 	time2 = System.nanoTime();
-	System.out.println("Runtime for a 1 element array: " + (time2 - time1) + " nanoseconds");
+	System.out.println("Runtime for a 2 element array: " + (time2 - time1) + " nanoseconds");
 
 	time1 = System.nanoTime();
 	merge.sort(test2);
 	time2 = System.nanoTime();
-	System.out.println("Runtime for a 10 element array: " + (time2 - time1) + " nanoseconds");
+	System.out.println("Runtime for a 4 element array: " + (time2 - time1) + " nanoseconds");
 
 	time1 = System.nanoTime();
 	merge.sort(test3);
 	time2 = System.nanoTime();
-	System.out.println("Runtime for a 100 element array: " + (time2 - time1) + " nanoseconds");
+	System.out.println("Runtime for a 8 element array: " + (time2 - time1) + " nanoseconds");
 
 	time1 = System.nanoTime();
 	merge.sort(test4);
 	time2 = System.nanoTime();
-	System.out.println("Runtime for a 1000 element array: " + (time2 - time1) + " nanoseconds");
+	System.out.println("Runtime for a 16 element array: " + (time2 - time1) + " nanoseconds");
 
 	time1 = System.nanoTime();
 	merge.sort(test5);
 	time2 = System.nanoTime();
-	System.out.println("Runtime for a 10000 element array: " + (time2 - time1) + " nanoseconds");
+	System.out.println("Runtime for a 32 element array: " + (time2 - time1) + " nanoseconds");
+
+	time1 = System.nanoTime();
+	merge.sort(test6);
+	time2 = System.nanoTime();
+	System.out.println("Runtime for a 64 element array: " + (time2 - time1) + " nanoseconds");
+
+	time1 = System.nanoTime();
+	merge.sort(test7);
+	time2 = System.nanoTime();
+	System.out.println("Runtime for a 128 element array: " + (time2 - time1) + " nanoseconds");
+
+	time1 = System.nanoTime();
+	merge.sort(test8);
+	time2 = System.nanoTime();
+	System.out.println("Runtime for a 2^10 element array: " + (time2 - time1) + " nanoseconds");
     }
 }
